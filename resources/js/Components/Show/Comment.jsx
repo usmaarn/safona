2262180571725b0a6f1b6393521acfb57fa6c11f
Usmaarn <usmaarn@gmail.com>
2022-08-27 11:15:43 +0100

    latest commit

--- resources/js/Components/Show/Comment.jsx
+++ resources/js/Components/Show/Comment.jsx
@@ -5,48 +5,53 @@ import date from '@/helpers/date'
 
 function Comment({ comment }) {
     return (
-        <div className="space-y-3 border-y p-3">
+        <div className="border-y p-3 flex gap-3">
 
-            <div className="flex items-center gap-2">
+            <div className="">
                 <div className="w-12 h-12 bg-zinc-500 rounded-full">
                     <img src="" alt="" />
                 </div>
-                <div>
-                    <h3>
-                        <a href="#">
-                            {comment.user.firstname + ' ' + comment.user.lastname}
-                        </a>
-                    </h3>
-                    <p className='text-p text-zinc-500'>
-                        {date.date_diff(comment.created_at)}
-                    </p>
-                </div>
-                <button className='ml-auto'>
-                    <BsThreeDots className='text-h3' />
-                </button>
             </div>
 
-            <p className='text-[1rem] font-light '>
-                {comment.body}
-            </p>
-
-            <div className="flex items-center gap-10">
-                <button className="flex items-center gap-1">
-                    <BsHeart className="text-[1.5rem]" />
-                    <span>33k</span>
-                </button>
-                <div className="flex items-center gap-1">
-                    <RiReplyLine className="text-[1.5rem]" />
-                    <span>34</span>
+            <div className="flex-grow flex flex-col gap-3">
+                <div className='flex justify-between items-center'>
+                    <div className="">
+                        <h3>
+                            <a href="#">
+                                {comment.user.firstname + ' ' + comment.user.lastname}
+                            </a>
+                        </h3>
+                        <p className='text-p text-zinc-500'>
+                            {date.date_diff(comment.created_at)}
+                        </p>
+                    </div>
+                    <button className=''>
+                        <BsThreeDots className='text-h3' />
+                    </button>
                 </div>
-                <button className='ml-auto'>Reply</button>
-            </div>
 
-            <div className="replies">
+                <p className='text-[1rem] font-light'>
+                    {comment.body}
+                </p>
+
+                <div className="flex items-center gap-10">
+                    <button className="flex items-center gap-1">
+                        <BsHeart className="text-[1.5rem]" />
+                        <span>33k</span>
+                    </button>
+                    <div className="flex items-center gap-1">
+                        <RiReplyLine className="text-[1.5rem]" />
+                        <span>34</span>
+                    </div>
+                    {/* <button className='ml-auto'>Reply</button> */}
+                </div>
+
+            </div>
+            {/* <div className="replies">
                 {[1].map(reply => (
                     <Reply reply={reply} key={reply} />
                 ))}
-            </div>
+            </div> */}
 
         </div>
 
